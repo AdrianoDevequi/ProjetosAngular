c6d0189b8f5d8f20e4205df6a4f997fa2ccd0308
adrianodevequi <adrianodevequi@gmail.com>
2018-04-10 15:35:18 -0300

    Progresso

--- angular/projetos/app1/src/app/painel/painel.component.ts
+++ angular/projetos/app1/src/app/painel/painel.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Frase } from '../shared/frase.model'
+
+import { FRASES } from './frases-mock'
+
 @Component({
   selector: 'app-painel',
   templateUrl: './painel.component.html',
@@ -7,9 +11,57 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PainelComponent implements OnInit {
 
-  constructor() { }
+  public frases: Frase[] = FRASES
+  public instrucao: string = 'Traduza a frase'
+  public resposta: string = ''
+
+  public rodada: number = 0
+  public rodadaFrase: Frase
+
+  public progresso: number = 0
+
+  constructor() {
+    this.atualizaRodada()
+    }
 
   ngOnInit() {
+  }
+
+  public atualizaResposta(resposta: Event): void{
+    
+    this.resposta = ( (<HTMLInputElement>resposta.target).value)
+
+   // console.log(this.resposta)
+  }
+
+  public verificarResposta(): void {
+    
+    if(this.rodadaFrase.frasePtBr == this.resposta){
+
+      alert('tradução Certa')
+      
+      //Trocar pergunta da rodada
+      this.rodada++
+      
+      this.progresso = this.progresso + ( 100 /this.frases.length )
+
+      //atualiza o objeto rodadaFrase
+      this.atualizaRodada()
+
+    }else{
+      alert('tradução errada')
+    }
+
+  } 
+
+  public atualizaRodada(): void{
+
+    //define a frase da rodada com base em alguma lógica
+    this.rodadaFrase = this.frases[this.rodada]
+     
+    //limpar a resposta
+    this.resposta = ''
+
   }
 
 }
